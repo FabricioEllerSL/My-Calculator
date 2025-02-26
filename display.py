153881a61af1234ec1732a0653cfdae8cad46d54
FabricioEllerSL <fabricioellersl@gmail.com>
2025-02-25 23:19:24 -0300

    :sparkles: feat: final main display configuration

--- display.py
+++ display.py
@@ -1,4 +1,6 @@
 from PySide6.QtWidgets import QLineEdit
+from utils.variables import BIG_FONT_SIZE, TEXT_MAGIN, MINIMUM_WIDTH
+from PySide6.QtCore import Qt
 
 class Display(QLineEdit):
     def __init__(self, *args, **kwargs):
@@ -6,4 +8,8 @@ class Display(QLineEdit):
         self.config_style()
         
     def config_style(self):
-        self.setStyleSheet("font-size: 1px")
+        self.setStyleSheet(F"font-size: {BIG_FONT_SIZE}px")
+        self.setMinimumHeight(BIG_FONT_SIZE * 1.5)
+        self.setMinimumWidth(MINIMUM_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(TEXT_MAGIN, TEXT_MAGIN, TEXT_MAGIN, TEXT_MAGIN)
